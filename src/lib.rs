e8ecd7440e944b5aaaba7921356ab56e9c3336d0
Branan Riley <branan@puppetlabs.com>
2015-07-14 12:03:35 -0700

    Even More hax

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,8 @@
-use std::fmt;
+#![feature(convert)]
 
-mod keybase;
-mod pgp;
+pub mod pgp;
 
-use self::pgp::PgpError;
-
-#[derive(Debug)]
-pub enum HedwigError {
-    Pgp(PgpError)
-}
-
-impl fmt::Display for HedwigError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            HedwigError::Pgp(ref err) => err.fmt(f)
-        }
-    }
-}
-
-impl From<PgpError> for HedwigError {
-    fn from(err: PgpError) -> HedwigError {
-        return HedwigError::Pgp(err);
-    }
-}
-
-pub type HedwigResult<T> = Result<T, HedwigError>;
-
-pub fn fetch_public_key(name: String) -> HedwigResult<pgp::PublicKey> {
-    Ok(try!(pgp::read_pubkey(keybase::fetch_user(name).public_key())))
+pub struct PublicKey {
+    n: Vec<u8>,
+    e: Vec<u8>
 }
