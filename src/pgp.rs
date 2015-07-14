62de595ed908dc267886d9b13429971a29afee39
Branan Riley <branan@puppetlabs.com>
2015-07-13 22:37:29 -0700

    Implement public key extraction from PGP armored blocks

--- src/pgp.rs
+++ src/pgp.rs
@@ -1,13 +1,16 @@
 extern crate rustc_serialize;
 use self::rustc_serialize::base64::{FromBase64, FromBase64Error};
 
+use super::{PublicKey, PrivateKey};
+
 use std::fmt;
 
 #[derive(Debug)]
 pub enum PgpError {
+    Armor,
     Crc,
-    CrcFormat,
-    Data,
+    Packet(&'static str),
+    Unsupported(&'static str),
     Base64(FromBase64Error)
 }
 
@@ -15,9 +18,10 @@ impl fmt::Display for PgpError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             PgpError::Base64(ref err) => err.fmt(f),
+            PgpError::Unsupported(msg) => write!(f, "Key uses unsupported packet format: {}", msg),
+            PgpError::Packet(msg) => write!(f, "Cannot read PGP packet: {}", msg),
+            PgpError::Armor => write!(f, "Armored file is malformed"),
             PgpError::Crc => write!(f, "CRC does not match"),
-            PgpError::CrcFormat => write!(f, "CRC is not the correct size"),
-            PgpError::Data => write!(f, "Expects two base64 blobs")
         }
     }
 }
@@ -30,7 +34,7 @@ impl From<FromBase64Error> for PgpError {
 
 pub type PgpResult<T> = Result<T, PgpError>;
 
-fn calc_crc(data: &Vec<u8>) -> u32 {
+fn calc_crc(data: &[u8]) -> u32 {
     let mut crc: u32 = 0x00B704CE;
     for b in data {
         let byte : u32 = (*b) as u32;
@@ -45,25 +49,115 @@ fn calc_crc(data: &Vec<u8>) -> u32 {
     crc & 0x00ffffff
 }
 
-fn read_armored(armored: String) -> PgpResult<Vec<u8>> {
-    let crc_start = armored.len() - 4;
-    let data_str = &armored[0..crc_start];
-    let crc_str = &armored[crc_start..armored.len()];
-    let data_bytes = try!(data_str.from_base64());
-    let crc_bytes = try!(crc_str.from_base64());
-    if crc_bytes.len() != 3 {
-        return Err(PgpError::CrcFormat);
+fn read_armored(armored: &str) -> PgpResult<Vec<u8>> {
+    let armor_headers = ["-----BEGIN PGP PUBLIC KEY BLOCK-----","-----BEGIN PGP PRIVATE KEY BLOCK-----"];
+    let armor_tails = ["-----END PGP PUBLIC KEY BLOCK-----","-----END PGP PRIVATE KEY BLOCK-----"];
+    let mut lines = armored.lines();
+
+    // Verify the header line
+    let header_line = lines.next().unwrap();
+    let packet_type_idx = match armor_headers.iter().position(|&x| x == header_line) {
+        Some(idx) => idx,
+        None => return Err(PgpError::Armor)
+    };
+
+    // Skip any header values - we don't care about them
+    while lines.next().unwrap() != "" {}
+
+    // Parse the body
+    let mut data_bytes : Vec<u8> = Vec::new();
+    let mut body_read = false;
+
+    while !body_read {
+        let line = lines.next().unwrap();
+        if line.len() == 0 {
+            return Err(PgpError::Armor);
+        }
+        if line.as_bytes()[0] == b'=' {
+            body_read = true;
+            let crc_bytes = try!(line[1..].from_base64());
+            let crc = ((crc_bytes[0] as u32) << 16) | ((crc_bytes[1] as u32) <<  8) | (crc_bytes[2] as u32);
+            if crc_bytes.len() != 3 {
+                return Err(PgpError::Armor);
+            }
+            if crc != calc_crc(data_bytes.as_slice()) {
+                return Err(PgpError::Crc);
+            }
+        } else {
+            data_bytes.extend(try!(line.from_base64()));
+        }
     }
-    let crc = ((crc_bytes[0] as u32) << 16) | ((crc_bytes[1] as u32) <<  8) | (crc_bytes[2] as u32);
-    if crc != calc_crc(&data_bytes) {
-        return Err(PgpError::Crc);
+    if lines.next().unwrap() != armor_tails[packet_type_idx] {
+        return Err(PgpError::Armor);
     }
     Ok(data_bytes)
 }
 
-pub fn read_pubkey(armored: String) -> PgpResult<()> {
+pub fn read_public_key(data : &[u8]) -> PgpResult<PublicKey> {
+    let tag = (data[0] >> 2) & 0x0f;
+    let len_type = data[0] & 0x03;
+    let style = (data[0] >> 6) & 0x01;
+    let check = (data[0] >> 7) & 0x01;
+    let mut next_idx = 1;
+
+    if check == 0 {
+        return Err(PgpError::Packet("Bad check bit"));
+    }
+
+    if style != 0 {
+        return Err(PgpError::Unsupported("New-style packet length"));
+    }
+
+    if tag != 6 {
+        return Err(PgpError::Packet("not a public key"));
+    }
+
+    if (len_type == 3) {
+        return Err(PgpError::Unsupported("Indeterminate length packets"));
+    }
+
+    let len_bytes = 1 << len_type;
+    let mut packet_length :usize = 0;
+    for _ in 0..len_bytes {
+        packet_length = packet_length << 8;
+        packet_length |= data[next_idx] as usize;
+        next_idx += 1;
+    }
+
+    let packet_data = &data[next_idx..(next_idx+packet_length)];
+
+    if packet_data[0] != 4 {
+        return Err(PgpError::Packet("Not a v4 key material packet"));
+    }
+
+    if packet_data[5] != 1 {
+        return Err(PgpError::Packet("Not an RSA key material packet"));
+    }
+
+    let n_mpi_start = 8;
+    let n_mpi_len = (((packet_data[6] as usize) << 8 | packet_data[7] as usize)+7) / 8;
+    let n_mpi_end = n_mpi_start + n_mpi_len;
+    let e_mpi_start = n_mpi_end + 2;
+    let e_mpi_len = (((packet_data[n_mpi_end] as usize) << 8 | packet_data[n_mpi_end+1] as usize)+7) /8;
+    let e_mpi_end = e_mpi_start + e_mpi_len;
+
+    assert_eq!(packet_length, e_mpi_end);
+    Ok(PublicKey {n: packet_data[n_mpi_start..n_mpi_end].to_owned(),
+                  e: packet_data[e_mpi_start..e_mpi_end].to_owned() })
+}
+
+pub fn read_armored_public_key(armored: &str) -> PgpResult<PublicKey> {
     let data = try!(read_armored(armored));
-    Ok(())
+    read_public_key(data.as_slice())
+}
+
+pub fn read_private_key(data: &[u8], password: Option<&str>) -> PgpResult<PrivateKey> {
+    Ok(PrivateKey { d: Vec::new(), p: Vec::new(), q: Vec::new(), u: Vec::new() })
+}
+
+pub fn read_armored_private_key(armored: &str, password: Option<&str>) -> PgpResult<PrivateKey> {
+    let data = try!(read_armored(armored));
+    read_private_key(data.as_slice(), password)
 }
 
 #[cfg(test)]
@@ -71,7 +165,76 @@ mod tests {
     use super::*;
 
     #[test]
-    fn read_pubkey_parses_armored() {
-        read_pubkey("mQINBE/f8fIBEACzWsBmCxY7O5NYa+Z/rJzJ0cy9k1KDoaY2G8tLiGHMcFJGQzUrhoH1ArRWAaOOWt6tLsvju4ZBMdy0NzTemFnUa9ZLpK8P1YlDwD6EBs1IUUWqKawa2u6uj4MAzrWo1DgYXOtPhSf8LrXIoygmBfTgNVKVH3nkHWNid2OlJK6mHh8RYtHSfGFl3k2uLgxeNkaqXvg/OSwHPBqZdQ3Qr+AhHo/1PbZqiQH2E/vP+jFul4grpY/CN7j0piV5iZ1WQ01Ozg34bAbXkiBQlxKzVWbVxNzaKz7Hl8bJ9Xhhik9QwnkHbjV+gv8Ximz0R0wXAoz0+EWsUzV+SV81v6C7EPLOVGfHEPGnFBf7giIDQCcIcFR/9W+tgcxzKO3CLMYBfNGsPuig8sgOJL+pNLYtBrAv2kbdIAx9ADx5wfaHnY9CtpfWKUaDM6qdBgvRx5IcpGX6qUFIz2kcZQbWGWFY7IyDpaIJHCbMvMntzBkImTRVcEJdd57IL5nxnUmWeEPXvjdM1hfS3RnBJ/ldnNmOsSlo+0dQTZpy5Xnpp7azpZV8edN0Vu9boOHrgE2PgDLdglQE6UrG9rBMgHw2ud497sIvRHiBXu9qs2Xzukfl5HkoGU0e+mrq7hoEpXi6EP6dGWsT3aBLCb9vPfWPEgBPWXW+vQKgkVg+hg6PEczbMoR3bQARAQABtCdCcmFuYW4gUHVydmluZS1SaWxleSA8YnJhbmFuQGdtYWlsLmNvbT6JAjgEEwECACIFAlElUyICGwMGCwkIBwMCBhUIAgkKCwQWAgMBAh4BAheAAAoJEO/6rCE4LNXKVIkP/3rMWK9OLqeju9IEZml9Qq+W6IXlqX/b31VdC6WiQ7hPJOZNc4Bf8QAtuO+hxk02rigZqPPHh2vGpjMDY2yLm3g2JMjNxX1OwMdaKYqp2FCXxpsvLDSVpCS2Y6MlRqkUcRqRYdVJVjFAPUSQA6UxJC41ZGx5+pVNX2m/zmroXLMUgrfLAlYRdAZw7klhiqdPeG8L1W69xx5hKHIoTZ3iHlpHikqajNNdcF1ycOmcng02lmr8+EEiYSb6q7udVfV10KrfKv+2cs3qFsBmjeAtOOEpGJZQ13wWQ17VigVKjyQ0447NgTHlRbBxdTTnWyMRyK2aEIxE8ZEx47hlZKxtgH83D5MjVBP3a/osyrmwzwrhBnB2euPQFTccpJeOE5XvYuRJz3OSN0bK25qKeDQD3mdlpwAKk33hChvCuz2VyGMcpjXR7Z2joGdtHlavzCbFhnNkU+THRbSmb7wkwRpBoApEfna7FspDQ4/uQOTBTJBy71k6eUz/wfEDcWxJBUhvw55B4QsqpySJAxByKHoFsbZxsaJ+2tK+BPUioVpHoZNGEJl4bNrNAsqQX8afS09IsoCIsG5HdDVzJ87+O/Nm2hLUK5dIErO802KhSZNpwwPwNu3wVh4uCcwMb9QRsO58QPpivP4mDwD4i/0bCvKSd7yF2MDcS7S8YoYEbbeRKVpbiQIcBBABAgAGBQJRJV7zAAoJEEaemzabDw8dDNUP/j8vsS9hOiTfdYUmMhWKwxQwPjpVyDSYBBvw/XJ4nmqoNcjmvmgkyMsiheuO1pOY6lGOWh3qdCP/J4mlMWJNObiGW9UCPJVG69meVLkU5o5RmCgHBgY18KlrLeq8L+qPqy7AqVtguTlSpSVCLzqCLaIJi8Vj7elg7K1tXlT4mmmNcTz2ifPoFqeDKeENxZP2cj6Jj0EzWh9vXTauu2bO6+plV5i3ec+BGmSpW/SeG+vTAY2MhYKEEbS0JAos6S6qkDF4KWEGD2GI4aZM4cmKUMWWycBDNI3TYSiUUKvdpErtDZKvMgAsg2rJU+dDMviMlMLfyTijd+d8li54jH2M1iJxFMkK09OEQudANDOdHr8riGGvqAEcV8qejrM7oMczVbIuHn9rQel0tji2CqPcUyZ4jgwmOCh2QyVAWRfJn1zVH1Zm1Sz6/LXWuA3hmBblOt/vYIvwzPUUgFu4frmFvvGC5vF4bMV5HgJ088D1glrPrlW3xKuc0pkSp4EQtsghoy794lLJ3l5EX0vQB3aWYv+dw+9E7RLPlV0OUw0XRMekLh499vQGWULOJGXKraca553sxQ7yjg2ttSHUgWxmJPofNM9qb7gXPHGiWra8FDdP7Udsps/MIAy/6v21WjzgFtS9ThyywpyVK3anpRH4Dh1/P3wL+9zLcamF/xkQXgWAtDVCcmFuYW4gUHVydmluZS1SaWxleSAoYnJhbmFuKSA8YnJhbmFuQHB1cHBldGxhYnMuY29tPokCOAQTAQIAIgUCT9/x8gIbAwYLCQgHAwIGFQgCCQoLBBYCAwECHgECF4AACgkQ7/qsITgs1cpi5g//dxeIMrLMD1+Z28Vd9kIAEuTjvVCC9oEGG+XeiFQce5EGX1UURlAnyzTn+J982UhisRW1pnxC6/bok7vvzYmC9uok+c4zcEJX/uUkHO13j223ZVGfhIBJYxudWWuVD6sfai23trNSsCiaraI6CIREuH1EGjGqWUvv2YtFRfct4C0tGgZIt3TdIBfLOy61xF2OV/iClhh3U1ex/iNUf+zb3mdaO9ARW98CUGAD6aRvu/2ujoqMB51AgA3G+I+2+6TkJLK/k4L4/IPIOgV+Vm8bY7tEx0eoB0CxvNydaChEgx2RR+WzeeejfuE8bijgxnRFtuC+pVAOdqQ4WoGQisjlmR9GdH1cDkUKDlibAGMpIZ36MrXtims2QTBuj+YFaKQ6JdRYCrHeYkP0A9WNvOzYJDpSrFkSUyVT4rknTXCUOmmjOumX0+Vd8I0Je//AC7qR6S5kgXHGeTxx/lwIucuCyNB1bgR4LFj4TFle+kVxz3VkGxpjPpCTxwbLbS9jm+8JrdclvZQo86bbU/lWfuPeiui4PFxbjOKHjNHjFwMngQXCMthKaPss0PyllZ5sAlzLOfIwWIymXj1hJFgB+ACaGUtCHrLn9Isr3UuWy+TVjTVcK6g3YNFwBwXqFP70O+IisGGuRiSHQY5LgmtIdRQ+KT6VLMBO03Jz9r0JRgE3+HuJARwEEAECAAYFAlElVXYACgkQHOtWPm8cRRk/+QgAqnjBT1WHPA8D3ip7uISkPakW9DZBoNRQ69I3G2zzwqKkp2lvTK3h6Fj9MBosP5THSQVukS2wjI5/NPLf7J0z40OUKg4Ayl6sO58lV9abWgp7RJTaLyPpnI/sy0pVk1QiyMYj9AxOWhDJgf4AnTBx+Y+VKhxapHlxDSVLg4/yLNJgFM87I3HLRwTfAHxhk109nQ7JO+anbSJW1Ji8dpEhvm63I04A043Zx/FfElLUJ55/wkQ1QAOHitjXmZX/7VWg684t09wd9YiK1W6ccIsbRUPvH1lkNCbD/hhrXm4iFIVy+XBT8981+/jcElvWXI9B8ONY50NdYni/w2c65HFqxIkCHAQQAQIABgUCUSVe8wAKCRBGnps2mw8PHX+kD/wPcJL001UoZRLfkgr5kio+JBIs1XssCsWYleQhkRDrJAUaCk4pVwj0NTmhoWE7g9eQkbFJZk96fCHpTKuJ01dR6bed+vY2ZgVPG/YTkbi1llqkwfxVFUrxGYnFr7kZGGmtqQZW2ePxUambUAetkUupbC79jgTIs4WG61f4NSxTzDd52W1VDPxx0lJbk2WVPjUDZC+BYJU7KC9B3e0oJUbqCpqcwtsG28F6i2ZZJuk8xr1+5tcHQT16YAA9zIgOIHBI2sUfiKIAm8+hLxeoXLthoS9Mrpm9ywVRT5t6VhaZkPwSlQh8d3RAKOh9A0VBF35pr6HV/r7Iz7t7B9ORNtuzGnhhRQIaQe8jG2Vbe8qu18AZLxE8eJDgrTPp2yaMuqcc3ZMHmGXAmyCrsg7F9xoA8MN6lPrDIkMKORWHGauunABrSm7YZc5Dx/yHV+3EYUy/Vo/2xQKNLY7FFKkSGldCs1E3qgRnlnQNxrjvmlMadLRdk3omP5+BNriLuMmZeTHQKx97lvYsPXEWEkdTwHTLuu31O9ik+OBkfePOY0Mkqv+GJ2rzKCLRXjawehcDGYhzkNkjEFPDDWN+8g+DCxm5ozini5P0ELJKvuLDDFY9e6vApNfjrMPMtxEyr4vRjy6Kcbg/huuAuLlGOFdqL1/QgRXEj9QYC2sQzaq937+5lbQuQnJhbmFuIFB1cnZpbmUtUmlsZXkgKGJyYW5hbikgPG1lQGJyYW5hbi5pbmZvPokCOAQTAQIAIgUCUbDngAIbAwYLCQgHAwIGFQgCCQoLBBYCAwECHgECF4AACgkQ7/qsITgs1cpLZxAAsJHm4FRCwzO6R+Bo1195cOxAgr85Wka2dibmdEDF2gMfx74E6+xHfBCxCVfxHkyDbGlvmtZ02CMkyj01ORDeK6gGQgvtCYGY2WoVB8Pb2L/W4ZwWH0kE/vA2h+a2anxSss6KBCYlaJulbQsD6vK//1Ci64O5SZzmpo3CNm2yDi3fAKFuDINAzHxOSinwwRFw7pYSHVzgNjfCfySdy1BRkmdYX1Xf5Tivf4xUbC2RCmm5JJgZl+yXKofepNeA0pXfK5AYjhhY55HzAbcaRhkiiZlvPMYHq+kG71K57pYQ4ePeSSklgeGH0qifXBLZ1XDIg2KqxupR4o4jo1N+H0XiM6JxaBFTIsE3io1TxLZR6BGRJwylX7tpwb7+xZaBfNiZbPdr/8ilZiqrGItUqG9Mxkbin/f6IYoiv0J2E5Nvfs6/md16FvXNYEXQAhylmRrqR8qs0jaHxziz3gQ2MAP+2zqG+MK7P7Nk0gdNYyCHVja5Gf6eBDdeRIUg8fmM+rd6tR5wxab4UX/3Z7zE4y3119W0db0gr4xusVOBcZy2Ug3vNCdRj3Sm8qb/+k3EooumeHxYRkSrmc6gPnLrMxRP5reHVnbfc/Fb8TSL5ihvMM0yfeOPqY9Wm8N1Ed/v741r9tzCSLk0qA+euaaP8RBv5gxnACCecTe7ofAuFWlQRS60JWtleWJhc2UuaW8vYnJhbmFuIDxicmFuYW5Aa2V5YmFzZS5pbz6JAi0EEwEKABcFAk/f8fICGwMDCwkHAxUKCAIeAQIXgAAKCRDv+qwhOCzVygnUEACOBjTZ6wC/ZlcvGbR6mQXvRbYFwCeKlMwzEGFjT9ZWC08PHuFBDtCatdvOtWji06BML/g4Mmd/yc+G5cAMOZItzrGukz+PhXiwsXM2IsYwoYRGjsMKWJCb+KZFFLZqO9DrjmsaL2R52+2DuN3I5K5xvmvpf/5DHzTcUV32UlXgOrLwvTKAgb1YP/ePM6NjQRtw9ckPhe7aGN0OXVUYthymqUpCYuju8mgThAHDZAGpb1o8nfr/Oaj3Qzi45S+OgTEBCYMQa5XU4nIcZkhXHJfrS319xy7bI98D1XIrQNa7j//OgNauD3GhbEI/R0vT+CaTH3/iekI4X+nqzBtBxd+J0omP+EvtWplt/psoPTgMebJRT2eAjPeFABarsOL2bno0tOqnOexzJIMtGHAxiAi2t2rZgTZvcNk+ZXhVoBv18J0gBaiWGWUmUJS+wkhQnpSY0HwXrbORM4InMoh5mq5OD4dIUJLt8GfFHYZXbhdEivGRDNx/wxkrzpZaOu8g14bnpd+Qal37deJ1gArl2DRTXK5E5qpf3EyeF/1fTTU8/tHZaAM2Xm9MBgA4pGXKko8jPv5vVyGQmMq3KP8hTq0tuwMlVg2wNj3lCBx1QxZ47uxXj1OPsdH9x2/52oFrsehWF6v1k4sJT7VkoNaNDt8RCMyrMceXK6pmxaHmtP+RMbkCDQRP3/HyARAAoVm2LY0fB5vf0Y79fEklgNOI76XGad3sr8bAm8be6ppz2/eVWuM0niPahTN27x1963pAN2i2pRkPUP9IJ5OQjNMgpX4oW/yCxXH0skOrDcPTXSMO2m6sglmIImqQGp1Qt0c7DWJ+hfyI819lUdn+nBV8UgQD8w5XpeqZ9ngqZhfSPojGpEcYDqkb+jVGkbzWcD4FIPlBGuj/uXwxjZ1UC/OkKo3Ef6S8ZRPtKaa7a+pJYHPrGAXijPuNUNJ4Fn/g+8iss+LcSN5wZbq03ZiHhK9lQfzgj0RH8sW8xE7eAw4Qr5+i9KWsP5qgFpX6FYuNIdqDpwE12STkyU9DG6ybpdMQW94WTsbRMmu1Hra63vgkOzspFCQhAGngqG2yRvLoUjrOK8oFKtpNuIKeeyiRNMx6/fmN8iPvHx7mJk6YYkXYRruNDun/O3qhy+5dONN8TecWJ+eXB3JRbDLNgTtIASP6Gnxkey5FA1W5VLlUGmQbW8hxNrcLw6jVnx7xYwF4LtsTZXRJYqh+6E7Abk8kpxaFAlR+Dz/bAGOf/IM/DP0sBPDL1YR/Nz7xpZ5EHDwFEnvsO/QizaV7mYyyraNjUmvgVfyh66CXTtIoYFWyn7HRdtFo5w+k/k4pZZqi0hI8I4N/Mp9PMRe7fw8Z7xSqOtTIB08wuegYPW2dopDGvAEAEQEAAYkCHwQYAQIACQUCT9/x8gIbDAAKCRDv+qwhOCzVyn9MEACGjg2mrHZ6zduHCBlik14Xq+A99JEh5EamPtXOC3zDgwwm8CmLyTWEFZVlq3J07oKSewLvastGqqbwbYBH1Iq5fDKH0MnYAsz7DNrrxq7af+KTI8OHVtG0eBSTYqoVMbUCwbgZYW/Vp8LVXYqVfXcw9c/bpkTm3681hGlWeLs6ZVTwJMClobF4b61FozQXvLjq0b/irC6OyWcIjEMp+90yZFFwoFc2i0AD6aoc9LuBms9cxaaZoz5dNQR8kNvKn9SiPA05YY3dkeu3gI5SRn8qySGM3unbzxFuGE/0iSFm+gHLCEjdY2A63biDwTFDF+puWDJzO6VpPQSnxlrjD2qdCAkL4MoJntUOcfjwrIfppBgMiWpQvOPupMFAaUEO4EHGiHElDUTWghIhiCFBFjjDwFpXRwhnclBI5VE20DmN1E7bmPEntm27yq8xz3jyvXwF5vOEZQ0QHmBZSTrMzQnogtyDJDwGJMo1yuiPlmePEouUIrrgYq4xxZjc7ucHS4P5rD86k+G1W9FDcGHH+GINH7KKApILh9mMeWOU9Er7t4/f42gNSjShlELSdXqNIir8yv+qe3cyt6UxQqJifdSHBYedj/A8AuVf6xpKLVDGxonD/sOP5u6rMAhX1uFYxLaJ7q84gevYT4GoryDaCsGBxHbCT3QMIs4N4+zooDx7vQ===o05J".to_string()).unwrap();
+    fn can_read_armored_public_key() {
+        let keydata ="-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: GnuPG v2
+
+mI0EVabG/AEEAKa4oAH9xQdSo9SAFmETpDpxsyvTTnwmqxhYDxllpqY1ZcEIiGB1
+URtCfvy1TFozi0rJHi/QPO2wdR+xvrpIi1d+9mJQKD2VhSrp18oio3/xqxoKf7Qc
+vzHGF46eCrMDn3LAXwCaQOJRY9lk4bUKjs5aHz6KAtC7XS9EddX5/5S/ABEBAAG0
+IEhlZHdpZyBUZXN0IDxoZWR3aWdAZXhhbXBsZS5jb20+iLcEEwEIACEFAlWmxvwC
+GwMFCwkIBwIGFQgJCgsCBBYCAwECHgECF4AACgkQgOC+ukVe0tNu/QP/UOS+Ybx7
+ED61M9Aa4jSCkrPayEgStqk+ZPct6M0J3QiyVAnoXrDn8jPzroV0zb6zr06XGqYC
+tf6qQmTcAO4dJSfT5BQ8dRUDWG8zM0qOU2ey1r6K3/SGGmZXozSA/b6OAJijfW48
++rVctsNvDvhl/52x1XItrIxnTtw2YvRe20u4jQRVpsb8AQQAqa+HkYNTcpaLeahh
+klg47XfExIWbYq2K/bmhMHLQFJ5v+5ySAFXdfMTTXDm9ghfvgmpALBa/5tj3P5Oe
+eHqnIglGzlw2E/GjGs/w826Q3Co5GmhJK+8ckKuFlboRa4zckdEQELfNe8L95OyG
+nv90JPRzKjtMttgDc7OxNxeheN0AEQEAAYifBBgBCAAJBQJVpsb8AhsMAAoJEIDg
+vrpFXtLTyl8D/j7dR4xtQz68wvrhl6yxdVnEtIcL7pAwl4fwUWBszBnIQU+sIOxL
+DuS2x8fODgRlJAxPGtGEEGLJ517lbS+oxqhTX4Z8qJXmPruXeUevUltlmJZ5Vi8F
+nSCSFbU63GPL7fOIuFFug7O9rcQen+aaDNZyd5SznwxVUStGEBDEockp
+=s6n/
+-----END PGP PUBLIC KEY BLOCK-----";
+        let expected_n = vec![166, 184, 160, 1, 253, 197, 7, 82, 163, 212, 128, 22, 97, 19, 164, 58, 113, 179, 43, 211, 78, 124, 38, 171, 24, 88, 15, 25, 101, 166, 166, 53, 101, 193, 8, 136, 96, 117, 81, 27, 66, 126, 252, 181, 76, 90, 51, 139, 74, 201, 30, 47, 208, 60, 237, 176, 117, 31, 177, 190, 186, 72, 139, 87, 126, 246, 98, 80, 40, 61, 149, 133, 42, 233, 215, 202, 34, 163, 127, 241, 171, 26, 10, 127, 180, 28, 191, 49, 198, 23, 142, 158, 10, 179, 3, 159, 114, 192, 95, 0, 154, 64, 226, 81, 99, 217, 100, 225, 181, 10, 142, 206, 90, 31, 62, 138, 2, 208, 187, 93, 47, 68, 117, 213, 249, 255, 148, 191];
+        let expected_e = vec![1, 0, 1];
+
+        let pubkey = read_armored_public_key(keydata).unwrap();
+        assert_eq!(pubkey.n.len(), 128);
+        assert_eq!(pubkey.n, expected_n);
+        assert_eq!(pubkey.e, expected_e);
+    }
+
+    #[test]
+    fn can_read_armored_private_key() {
+        let privkey = read_armored_private_key("-----BEGIN PGP PRIVATE KEY BLOCK-----
+Version: GnuPG v2
+
+lgAAAgYEVabG/AEEAKa4oAH9xQdSo9SAFmETpDpxsyvTTnwmqxhYDxllpqY1ZcEI
+iGB1URtCfvy1TFozi0rJHi/QPO2wdR+xvrpIi1d+9mJQKD2VhSrp18oio3/xqxoK
+f7QcvzHGF46eCrMDn3LAXwCaQOJRY9lk4bUKjs5aHz6KAtC7XS9EddX5/5S/ABEB
+AAH+BwMC6yKaPW6t1KDr4DT7PhWI2Zgq7mL3Z7j6l/1mDHSrKrfMXHgcQmMWooD0
+jig5v69jkhfsxr/UFG83zlH76Gs1P8G9lN8MJHY5fpJV46PERXwodRnIgCKvq6DH
+vcSqzb+rYPbl5oSqDssaqOMroaFk7DOPyVF2rHF/2GP8zY1bWdQvligc+ZaqJQnW
+jRtN2JZj5ZEbCVxXzFyDTn83393Uzy13dH+cJrgp3re2IRqRXvYSAHhxG2m6hBNE
+c/GwRe1ZEffZwazCdfYjiShKFMtqWuA+8L/0AvoNdSEXUWsGAX941Rb+IwmK+/8O
+tlHI6PW28tfuRUPunkTVHo28Ltfcb5MVtu6NURyl6SKI0bqB05XG9EvFi2sVHbs1
+1+MVWI6gog69Ju5CcRCKPxyEqICBMYVW79ec2GeURjNBiFFiN0uXLqDG3PCMEk/V
+YiM55r6voRRc80S4mXvo8phkmSosfcgWZhJnAD8zgjxeyF8V567/ExBF0bQgSGVk
+d2lnIFRlc3QgPGhlZHdpZ0BleGFtcGxlLmNvbT6ItwQTAQgAIQUCVabG/AIbAwUL
+CQgHAgYVCAkKCwIEFgIDAQIeAQIXgAAKCRCA4L66RV7S0279A/9Q5L5hvHsQPrUz
+0BriNIKSs9rISBK2qT5k9y3ozQndCLJUCehesOfyM/OuhXTNvrOvTpcapgK1/qpC
+ZNwA7h0lJ9PkFDx1FQNYbzMzSo5TZ7LWvorf9IYaZlejNID9vo4AmKN9bjz6tVy2
+w28O+GX/nbHVci2sjGdO3DZi9F7bS54AAAIGBFWmxvwBBACpr4eRg1Nylot5qGGS
+WDjtd8TEhZtirYr9uaEwctAUnm/7nJIAVd18xNNcOb2CF++CakAsFr/m2Pc/k554
+eqciCUbOXDYT8aMaz/DzbpDcKjkaaEkr7xyQq4WVuhFrjNyR0RAQt817wv3k7Iae
+/3Qk9HMqO0y22ANzs7E3F6F43QARAQAB/gcDAjZ0faZ+S1VA66PeQCcCV1JfXo1X
+zW+/h2quL8NnzyBfX+5vsiUhW5udvgKiNiHDvuG/8nnyxYjLCfndsiPnUNdhoB7S
+ZBYhQwqSFujWm2rvLwbyyu0fxYhtcqVXhnJA7hjx6heQplbB5n3h2UkXHah41MKf
++1JGhOIPFMtLg9TadmrJcxApeBm86pzhFd5wCbCSM0DM3GIUyXNR5cq4Wq/E14Lp
+OlQae7vMdmOBSI4kTPgqOUVGZSKpDxLnemYpeCgvwiDfSH4fmIeL6aIXSGgK1UN7
+M0nzq+LpKrJq1k7LuDjWWwKGOKyrXBMzXDtgi5I2FLp0YUXTxQIT3Iii33yYce3m
+X0uPEiPzzJ0zljT/mgzZ/I7t6yox1nWguH+WsRmt2w83KNg/UsjEQG63RfshCQb+
+4Yfv7MT6loDw77aESVrg1KYZBEEEbEbZAUAQpBHyPkdnruYlhPuWcqS0TWcC3dkj
+1NhNRIQAjbr3TkBQ5xxRzz+InwQYAQgACQUCVabG/AIbDAAKCRCA4L66RV7S08pf
+A/4+3UeMbUM+vML64ZessXVZxLSHC+6QMJeH8FFgbMwZyEFPrCDsSw7ktsfHzg4E
+ZSQMTxrRhBBiyede5W0vqMaoU1+GfKiV5j67l3lHr1JbZZiWeVYvBZ0gkhW1Otxj
+y+3ziLhRboOzva3EHp/mmgzWcneUs58MVVErRhAQxKHJKQ==
+=tKsK
+-----END PGP PRIVATE KEY BLOCK-----", Some("password")).unwrap();
+
+        assert_eq!(privkey.d, []);
+        assert_eq!(privkey.p, []);
+        assert_eq!(privkey.q, []);
+        assert_eq!(privkey.u, []);
     }
 }
